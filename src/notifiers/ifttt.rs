521a93a975fc75a3d95d956fc61d021e64156ecd
nipil <nipil@users.noreply.github.com>
2023-01-14 19:55:07 +0100

    handle webhook variants

--- src/notifiers/ifttt.rs
+++ src/notifiers/ifttt.rs
@@ -9,6 +9,9 @@ use super::{NotifierFactoryTrait, NotifierTrait};
 
 // IFTTT WEBHOOK implementation
 
+/// Common environment variable to select the webhook input variant.
+const ENV_NAME_IFTTT_WEBHOOK_VARIANT: &str = "IFTTT_WEBHOOK_VARIANT";
+
 /// Common environment variable to select the webhook event.
 const ENV_NAME_IFTTT_WEBHOOK_EVENT: &str = "IFTTT_WEBHOOK_EVENT";
 
@@ -27,37 +30,73 @@ struct IftttApiError {
     errors: Vec<IftttApiErrorMessage>,
 }
 
+/// Variant selecting the structure of the API input.
+enum WebHookVariant {
+    Value,
+    Json,
+}
+
 /// Holds the user credentials and event identifier used with the API.
 pub struct WebHook {
+    variant: WebHookVariant,
     event: String,
     key: String,
 }
 
 impl WebHook {
     /// Builds a new instance.
-    fn new(event: &str, key: &str) -> WebHook {
-        let event = event.trim();
+    fn new(variant: &str, event: &str, key: &str) -> WebHook {
+        let variant = variant.trim();
+        if variant.is_empty() {
+            panic!("Ifttt webhook variant shoult not be empty");
+        }
+        let variant = match variant {
+            "value" => WebHookVariant::Value,
+            "json" => WebHookVariant::Json,
+            // TODO: return error instead of panicking
+            _ => panic!("Invalid ifttt webhook variant"),
+        };
+
+        // TODO: sanitize inputs according to notifier API format spec.
+        let event = event.trim().to_string();
         if event.is_empty() {
+            // TODO: return error instead of panicking
             panic!("Ifttt webhook event should not be empty");
         }
-        let key = key.trim();
+        let key = key.trim().to_string();
         if key.is_empty() {
+            // TODO: return error instead of panicking
             panic!("Ifttt webhook key shoult not be empty");
         }
-        // TODO: sanitize inputs according to notifier API format spec
+
         WebHook {
-            event: event.to_string(),
-            key: key.to_string(),
+            event,
+            key,
+            variant,
         }
+    }
+
+    /// Builds ifttt URL according to selected variant.
+    fn get_url(&self) -> String {
+        format!(
+            // Common URL to send queries to IFTTT webhook
+            // - the first placeholder is for the event name
+            // - the second placeholder is for the variant (empty or `/json`)
+            // - the third placeholder is for the user's key
+            "https://maker.ifttt.com/trigger/{}{}/with/key/{}",
+            self.event,
+            match self.variant {
+                WebHookVariant::Value => "",
+                WebHookVariant::Json => "json/",
+            },
+            self.key
+        )
     }
 
     /// Sends the actual API request.
     // TODO: add a selector for either json or valueX api
     fn query(&self, params: HashMap<&str, &str>) -> Result<Response, LibError> {
-        let url = format!(
-            "https://maker.ifttt.com/trigger/{}/json/with/key/{}",
-            self.event, self.key
-        );
+        let url = self.get_url();
         let client = reqwest::blocking::Client::new();
         let response = client
             .post(url)
@@ -97,9 +136,10 @@ impl WebHook {
 impl NotifierFactoryTrait for WebHook {
     /// Builds a WebHook notifier from environment variables.
     fn from_env() -> Result<Box<dyn NotifierTrait>, LibError> {
+        let variant = crate::get_env_var(ENV_NAME_IFTTT_WEBHOOK_VARIANT)?;
         let event = crate::get_env_var(ENV_NAME_IFTTT_WEBHOOK_EVENT)?;
         let key = crate::get_env_var(ENV_NAME_IFTTT_WEBHOOK_KEY)?;
-        Ok(Box::new(WebHook::new(&event, &key)))
+        Ok(Box::new(WebHook::new(&variant, &event, &key)))
     }
 }
 
@@ -107,7 +147,16 @@ impl NotifierTrait for WebHook {
     /// Sends an notification using the provided data.
     fn notify(&self, content: &str) -> Result<(), LibError> {
         // build request
-        let params = HashMap::from([("available", content)]);
+        // TODO: handle variants
+        let mut params = HashMap::new();
+        match self.variant {
+            WebHookVariant::Value => {
+                params.insert("value1", content);
+            }
+            WebHookVariant::Json => {
+                params.insert("available", content);
+            }
+        }
 
         let response = self.query(params)?;
         Ok(())
@@ -116,10 +165,16 @@ impl NotifierTrait for WebHook {
     /// Tests by sending a notification with dummy values.
     fn test(&self) -> Result<(), LibError> {
         let mut params = HashMap::new();
-        params.insert("value1", "foo");
-        params.insert("value2", "bar");
-        params.insert("value3", "baz");
-        params.insert("no", "way");
+        match self.variant {
+            WebHookVariant::Value => {
+                params.insert("value1", "foo");
+                params.insert("value2", "bar");
+                params.insert("value3", "baz");
+            }
+            WebHookVariant::Json => {
+                params.insert("dummy", "content");
+            }
+        }
 
         let response = self.query(params)?;
         Ok(())
