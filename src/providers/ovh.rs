nipil/dedicated-server-availability-watcher

commit 7e9586fcd825163e8b1951767e47b5ebc8665c9d

[FIX] do not reject multiple results for the same server

src/providers/ovh.rs
@@ -173,20 +173,8 @@ impl ProviderTrait for Ovh {
 
     /// Checks provider for the availability of a given server type.
     fn check(&self, server: &str) -> Result<bool, LibError> {
-        let mut results = self.api_get_dedicated_server_datacenter_availabilities(Some(server))?;
-
-        match results.pop() {
-            None => Err(LibError::UnknownServer {
-                server: server.to_string(),
-            }),
-            Some(result) => {
-                results
-                    .is_empty()
-                    .then_some(result.is_available())
-                    .ok_or(LibError::ApiError {
-                        message: format!("Multiple references found for server {server}"),
-                    })
-            }
-        }
+        let results = self.api_get_dedicated_server_datacenter_availabilities(Some(server))?;
+        // Server ids can have duplicates (location, specs, ...)
+        Ok(!results.is_empty())
     }
 }

commit cb227e30d102de0b940100aa971630c920945482

[FIX] regression in 7e9586f: check for actual availability

src/providers/ovh.rs
@@ -175,6 +175,11 @@ impl ProviderTrait for Ovh {
     fn check(&self, server: &str) -> Result<bool, LibError> {
         let results = self.api_get_dedicated_server_datacenter_availabilities(Some(server))?;
         // Server ids can have duplicates (location, specs, ...)
-        Ok(!results.is_empty())
+        for result in results {
+            if result.is_available() {
+                return Ok(true)
+            }
+        }
+        Ok(false)
     }
 }